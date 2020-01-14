ea0b22220fb206814c6fb4c85f9cee95bcda1d31
mumane <59897032+mumane@users.noreply.github.com>
2020-01-14 16:57:42 -0600

    Add files via upload

--- Group5_Lab2/prob2A/main/main.java
+++ Group5_Lab2/prob2A/main/main.java
@@ -1,1 +1,24 @@
+package prob2A.main;
 
+import prob2A.GradeReport;
+import prob2A.Student;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GradeReport gradeReport=new GradeReport("A");
+		Student s1= gradeReport.getStudent();
+		s1.setName("John");
+		GradeReport gradeReport2=new GradeReport("C");
+		Student s2=gradeReport2.getStudent();
+		s2.setName("Mark");
+	
+		System.out.println("------Report Grade-----");
+		System.out.println(gradeReport);
+		System.out.println(gradeReport2);
+	
+		
+	}
+
+}
