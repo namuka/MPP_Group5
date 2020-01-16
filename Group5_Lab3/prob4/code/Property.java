158923bf0a590cfe9409b6a2953cb2325bea7740
namuka <s.namuun88@gmail.com>
2020-01-16 01:15:42 -0600

    Update Property.java

--- Group5_Lab3/prob4/code/Property.java
+++ Group5_Lab3/prob4/code/Property.java
@@ -4,23 +4,17 @@ public class Property {
 	private double rent;
 	private Address address;
 	
+	public Property() {
+		address = new Address();
+	}
+	
 	public double computeRent() {
 		return rent;
-	}
-	
-	public void createAddress(String c) {
-		this.address = new Address(c);
-	}
-	
-	public void createAddress(String s, String c, String st, String z) {
-		this.address = new Address(s, c, st, z);
 	}
 	
 	public Address getAddress() {
 		return address;
 	}
-	public String getCity() {
-		return address.getCity();
-	}
+	
 	
 }
