e45804d7816f25940e1abe29b7b985c330226161
mumane <59897032+mumane@users.noreply.github.com>
2020-01-18 16:16:49 -0600

    Add files via upload

---
+++ Group5_Lab5/prob4/extpackage/Main.java
@@ -0,0 +1,24 @@
+package prob4.extpackage;
+
+import java.time.LocalDate;
+
+import prob4.CustOrderFactory;
+import prob4.Customer;
+import prob4.Order;
+
+public class Main {
+	public static void main(String[] args) {
+		Customer cust = CustOrderFactory.createCustomer("Bob");
+		Order order = Order.newOrder(cust, LocalDate.now());
+		order.addItem("Shirt");
+		order.addItem("Laptop");
+
+		order = Order.newOrder(cust, LocalDate.now());
+		order.addItem("Pants");
+		order.addItem("Knife set");
+
+		System.out.println(cust.getOrders());
+	}
+}
+
+		
